035609101d45f2de3f5b2654f15fed499e179127
Ernestso1998 <135281894+Ernestso1998@users.noreply.github.com>
2023-06-10 19:10:32 -0500

    Update main.cpp

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,16 @@
 ﻿//write include statements
-
+#include <iostream>
+using namespace std;
 
 //write namespace using statement for cout
 
 
 int main()
 {
+	int num;
+	cin >> num;
 	
+	int result
 
 	return 0;
 }
