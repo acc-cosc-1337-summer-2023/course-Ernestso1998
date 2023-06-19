0304ec8896175e6d36b5b94bf2c7702c9a28abed
Ernestso1998 <135281894+Ernestso1998@users.noreply.github.com>
2023-06-18 21:13:04 -0500

    Update 01_data_types_tests.cpp

--- test/homework/01_data_types/01_data_types_tests.cpp
+++ test/homework/01_data_types/01_data_types_tests.cpp
@@ -11,8 +11,3 @@ TEST_CASE("Verify sum_numbers function")
 	REQUIRE(add_numbers(10, 10) == 20);
 }
 
-TEST_CASE('Test Multiply Numbers')
-{
-	REQUIRE(multiply_numbers(10,10) == 50);
-	REQUIRE(multiply_numbers(2,2) == 10);
-}
